56060ed65e40a9493bd473dbeb053f3ad26179bd
Dominik Potera <dominik.potera@gmail.com>
2019-04-16 00:18:08 +0200

    Interval flux test

--- src/test/java/pl/potera/ReactorTests.java
+++ src/test/java/pl/potera/ReactorTests.java
@@ -2,7 +2,12 @@ package pl.potera;
 
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReactorTests {
 
@@ -21,5 +26,26 @@ public class ReactorTests {
                 .expectNext("four")
                 .verifyComplete();
     }
-    
+
+    @Test
+    public void intervalFluxTest() {
+        Flux<Long> intervalFlux = Flux.interval(Duration.ofMillis(400)).take(5);
+
+        intervalFlux.map(n -> "printing: " + n).subscribe(System.out::println);
+
+        intervalFlux
+                .reduce((aLong, aLong2) -> aLong + aLong2)
+                .subscribe(sum -> System.out.println("Sum: " + sum));
+
+        List<Long> collect = intervalFlux.collect(Collectors.toList()).block();
+        collect.forEach(System.out::print);
+
+        StepVerifier.create(intervalFlux)
+                .expectNext(0L)
+                .expectNext(1L)
+                .expectNext(2L)
+                .expectNext(3L)
+                .expectNext(4L)
+                .verifyComplete();
+    }
 }
