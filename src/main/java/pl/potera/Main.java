f9cd559b8dd5292284219dbdfedc528f01187ac9
Dominik Potera <dominik.potera@gmail.com>
2019-04-17 00:09:45 +0200

    Imperative and Reactive code example

---
+++ src/main/java/pl/potera/Main.java
@@ -0,0 +1,19 @@
+package pl.potera;
+
+import reactor.core.publisher.Mono;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "Dominik";
+        String capitalName = name.toUpperCase();
+        String greeting = "Hello " + capitalName + "!";
+        System.out.println(greeting);
+
+
+        Mono.just("Dominik")
+                .map(String::toUpperCase)
+                .map(str -> "Hello " + str + "!")
+                .subscribe(System.out::println);
+    }
+}
