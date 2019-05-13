33ff7ada94f0490154a0236f403cdfa817970641
Dominik Potera <dominik.potera@gmail.com>
2019-05-13 17:43:03 +0200

    new subproject with simple crud example

---
+++ crud-controller/src/main/java/pl/potera/crudcontroller/CrudControllerApplication.java
@@ -0,0 +1,13 @@
+package pl.potera.crudcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudControllerApplication.class, args);
+	}
+
+}
